67362fd6f64a497a7bdb3b8e45d7649b7c4925ad
noname <77149479+Utopi-L@users.noreply.github.com>
2024-02-09 10:52:33 +0800

    new file:   web exam/web f/1-1.html 	new file:   web exam/web f/1-1_files/04-chateauscope_yankodesign-510x314.jpg 	new file:   web exam/web f/1-1_files/09_makutrailer_tailand_trailerradio-510x314.jpg 	new file:   web exam/web f/1-1_files/18_2ndhome_sanuadx_timberframecabin-510x314.jpg 	new file:   web exam/web f/1-1_files/Dreamhome_architecture_unfoldingdesign-510x314.jpg 	new file:   web exam/web f/1-1_files/Good_Design_Selection_Award.webp 	new file:   "web exam/web f/1-1_files/VYgtqqKkHeeSn6NZMlhk9f9ji4vf9cWE8BpKc7mmgB8.js.\344\270\213\350\275\275" 	new file:   "web exam/web f/1-1_files/ad_status.js.\344\270\213\350\275\275" 	new file:   "web exam/web f/1-1_files/ajax-load.js.\344\270\213\350\275\275" 	new file:   web exam/web f/1-1_files/all-terrain_ebike_with_power_of_1000_watts_hero-690x458.webp 	new file:   "web exam/web f/1-1_files/analytics.js.\344\270\213\350\275\275" 	new file:   web exam/web f/1-1_files/autoptimize_d9f7d44f3c82dafb8686208ad556a480.css 	new file:   "web exam/web f/1-1_files/base.js.\344\270\213\350\275\275" 	new file:   "web exam/web f/1-1_files/cast_sender.js(1).\344\270\213\350\275\275" 	new file:   "web exam/web f/1-1_files/cast_sender.js.\344\270\213\350\275\275" 	new file:   web exam/web f/1-1_files/css 	new file:   "web exam/web f/1-1_files/embed.js.\344\270\213\350\275\275" 	new file:   web exam/web f/1-1_files/f(1).txt 	new file:   web exam/web f/1-1_files/f.txt 	new file:   "web exam/web f/1-1_files/fetch-polyfill.js.\344\270\213\350\275\275" 	new file:   "web exam/web f/1-1_files/frontend-gtag.min.js.\344\270\213\350\275\275" 	new file:   "web exam/web f/1-1_files/gpt.js.\344\270\213\350\275\275" 	new file:   web exam/web f/1-1_files/ifdesign_logo4.webp 	new file:   web exam/web f/1-1_files/in.php 	new file:   "web exam/web f/1-1_files/jpibfi.client.js.\344\270\213\350\275\275" 	new file:   "web exam/web f/1-1_files/jquery-migrate.min.js.\344\270\213\350\275\275" 	new file:   "web exam/web f/1-1_files/jquery.assets.js.\344\270\213\350\275\275" 	new file:   "web exam/web f/1-1_files/jquery.caroufredsel.js.\344\270\213\350\275\275" 	new file:   "web exam/web f/1-1_files/jquery.custom.js.\344\270\213\350\275\275" 	new file:   "web exam/web f/1-1_files/jquery.flexslider-min.js.\344\270\213\350\275\275" 	new file:   "web exam/web f/1-1_files/jquery.min.js.\344\270\213\350\275\275" 	new file:   web exam/web f/1-1_files/js 	new file:   web exam/web f/1-1_files/js(1) 	new file:   web exam/web f/1-1_files/js(2) 	new file:   "web exam/web f/1-1_files/lazysizes.min.js.\344\270\213\350\275\275" 	new file:   web exam/web f/1-1_files/maldives_floating_city_1.jpg 	new file:   web exam/web f/1-1_files/maldives_floating_city_2.webp 	new file:   web exam/web f/1-1_files/maldives_floating_city_3.webp 	new file:   web exam/web f/1-1_files/maldives_floating_city_4.webp 	new file:   web exam/web f/1-1_files/pitaka_ecosystem_2022_event_1-510x314.webp 	new file:   web exam/web f/1-1_files/pitaka_ecosystem_2022_event_1-690x460.webp 	new file:   "web exam/web f/1-1_files/pubads_impl_2022062201.js.\344\270\213\350\275\275" 	new file:   web exam/web f/1-1_files/reddot_logo4.webp 	new file:   "web exam/web f/1-1_files/remote.js.\344\270\213\350\275\275" 	new file:   "web exam/web f/1-1_files/script.min.js.\344\270\213\350\275\275" 	new file:   web exam/web f/1-1_files/translate_24dp.png 	new file:   web exam/web f/1-1_files/translateelement.css 	new file:   "web exam/web f/1-1_files/www-embed-player.js.\344\270\213\350\275\275" 	new file:   web exam/web f/1-1_files/www-player.css 	new file:   web exam/web f/1-1_files/yd_logomark_retina.webp 	new file:   web exam/web f/1-1_files/z4Zo5Nr7Afc.html

---
+++ web exam/web f/1-1_files/in.php
@@ -0,0 +1,8 @@
+// (☞ﾟ∀ﾟ)☞ static33
+if( window._cgen ) {
+_cgen.set_cookie('_jsuid', '198486476');
+_cgen.advanced();
+_cgen.ping_start();
+_cgen.set_cookie('_heatmaps_g2g_101265524', 'no', 600 );
+_heatmaps_g2g_101265524='no';
+}
