73f2cb7b993d7a6851980c473ec9755aaedb186a
noname <77149479+Utopi-L@users.noreply.github.com>
2024-04-12 14:36:29 +0800

    new file:   Windows Program Design/Test 2/TestTwo.h

---
+++ Windows Program Design/Test 2/TestTwo.h
@@ -0,0 +1,32 @@
+﻿
+// TestTwo.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CTestTwoApp:
+// 有关此类的实现，请参阅 TestTwo.cpp
+//
+
+class CTestTwoApp : public CWinApp
+{
+public:
+	CTestTwoApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTestTwoApp theApp;
