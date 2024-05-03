11f02a87dfa970af9d57f53680a13ef816553e60
noname <77149479+Utopi-L@users.noreply.github.com>
2024-05-03 15:52:32 +0800

    new file:   Windows Program Design/Test 3/TestThree 3.h

---
+++ Windows Program Design/Test 3/TestThree 3.h
@@ -0,0 +1,40 @@
+﻿
+// TestThree 3.h: TestThree 3 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CTestThree3App:
+// 有关此类的实现，请参阅 TestThree 3.cpp
+//
+
+class CTestThree3App : public CWinAppEx
+{
+public:
+	CTestThree3App() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	UINT  m_nAppLook;
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTestThree3App theApp;
