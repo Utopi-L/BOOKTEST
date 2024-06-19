664613ca7f9c8f9e898767e3c525638f5cd825c9
noname <77149479+Utopi-L@users.noreply.github.com>
2024-06-19 14:21:41 +0800

    new file:   Windows Program Design/Test 4/5-1.h 	new file:   Windows Program Design/Test 4/5-1Doc.cpp

---
+++ Windows Program Design/Test 4/5-1.h
@@ -0,0 +1,33 @@
+﻿
+// 实验5-1.h: 实验5-1 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// C实验51App:
+// 有关此类的实现，请参阅 实验5-1.cpp
+//
+
+class C实验51App : public CWinApp
+{
+public:
+	C实验51App() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern C实验51App theApp;
