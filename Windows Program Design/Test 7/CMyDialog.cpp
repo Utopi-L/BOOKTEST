27229178055348c6e503a515e5b7b8e9105bee20
noname <77149479+Utopi-L@users.noreply.github.com>
2024-08-08 09:10:19 +0800

    new file:   Windows Program Design/Test 7/CMyDialog.cpp

---
+++ Windows Program Design/Test 7/CMyDialog.cpp
@@ -0,0 +1,58 @@
+﻿// CMyDialog.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "实验6-3.h"
+#include "afxdialogex.h"
+#include "CMyDialog.h"
+
+
+// CMyDialog 对话框
+
+IMPLEMENT_DYNAMIC(CMyDialog, CDialog)
+
+CMyDialog::CMyDialog(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_DIALOG1, pParent)
+{
+
+}
+
+CMyDialog::~CMyDialog()
+{
+}
+
+void CMyDialog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CMyDialog, CDialog)
+	ON_EN_CHANGE(IDC_EDIT1, &CMyDialog::OnEnChangeEdit1)
+	ON_EN_CHANGE(IDC_EDIT2, &CMyDialog::OnEnChangeEdit2)
+END_MESSAGE_MAP()
+
+
+// CMyDialog 消息处理程序
+
+
+void CMyDialog::OnEnChangeEdit1()
+{
+	// TODO:  如果该控件是 RICHEDIT 控件，它将不
+	// 发送此通知，除非重写 CDialog::OnInitDialog()
+	// 函数并调用 CRichEditCtrl().SetEventMask()，
+	// 同时将 ENM_CHANGE 标志“或”运算到掩码中。
+
+	// TODO:  在此添加控件通知处理程序代码
+}
+
+
+void CMyDialog::OnEnChangeEdit2()
+{
+	// TODO:  如果该控件是 RICHEDIT 控件，它将不
+	// 发送此通知，除非重写 CDialog::OnInitDialog()
+	// 函数并调用 CRichEditCtrl().SetEventMask()，
+	// 同时将 ENM_CHANGE 标志“或”运算到掩码中。
+
+	// TODO:  在此添加控件通知处理程序代码
+}
