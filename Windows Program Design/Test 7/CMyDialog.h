ce4d65adbd63a3275c3510fa5da85d6edc157ca5
noname <77149479+Utopi-L@users.noreply.github.com>
2024-08-09 09:37:15 +0800

    new file:   Windows Program Design/Test 7/CMyDialog.h

---
+++ Windows Program Design/Test 7/CMyDialog.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// CMyDialog 对话框
+
+class CMyDialog : public CDialog
+{
+	DECLARE_DYNAMIC(CMyDialog)
+
+public:
+	CMyDialog(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~CMyDialog();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG1 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnEnChangeEdit1();
+	afx_msg void OnEnChangeEdit2();
+};
