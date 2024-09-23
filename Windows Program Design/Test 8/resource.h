b608dcfa0e59d0d868971d3b4161b38d4780f2d8
Utopi-L <77149479+Utopi-L@users.noreply.github.com>
2024-09-23 08:20:40 +0800

    new file:   Windows Program Design/Test 8/resource.h

---
+++ Windows Program Design/Test 8/resource.h
@@ -0,0 +1,28 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 My71.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MY71_DIALOG                 102
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       129
+#define IDR_DRAW                        129
+#define ID_Menu                         32771
+#define ID_Menu32772                    32772
+#define ID_Menu32773                    32773
+#define ID_1                            32774
+#define ID_Menu32775                    32775
+#define ID__32776                       32776
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
