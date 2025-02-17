0c3b253ce8b2f5bf404d88c4b84c4a259a19dbd4
Utopi-L <77149479+Utopi-L@users.noreply.github.com>
2025-02-17 13:26:49 +0800

    renamed:    "Windows Program Design/Test 9/\345\256\236\351\252\2147-2.cpp" -> Windows Program Design/Test 9/7-2.cpp 	renamed:    "Windows Program Design/Test 9/\345\256\236\351\252\2147-2.h" -> Windows Program Design/Test 9/7-2.h 	new file:   Windows Program Design/Test 9/7-2Doc.cpp

---
+++ Windows Program Design/Test 9/7-2Doc.cpp
@@ -0,0 +1,141 @@
+﻿
+// 实验7-2Doc.cpp: C实验72Doc 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "实验7-2.h"
+#endif
+
+#include "实验7-2Doc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// C实验72Doc
+
+IMPLEMENT_DYNCREATE(C实验72Doc, CDocument)
+
+BEGIN_MESSAGE_MAP(C实验72Doc, CDocument)
+END_MESSAGE_MAP()
+
+
+// C实验72Doc 构造/析构
+
+C实验72Doc::C实验72Doc() noexcept
+{
+	/*m_rect.left = 50;
+	m_rect.right = 150;
+	m_rect.top = 50;
+	m_rect.bottom = 150;*/ // TODO: 在此添加一次性构造代码
+
+}
+
+C实验72Doc::~C实验72Doc()
+{
+}
+
+BOOL C实验72Doc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: 在此添加重新初始化代码
+	// (SDI 文档将重用该文档)
+
+	return TRUE;
+}
+
+
+
+
+// C实验72Doc 序列化
+
+void C实验72Doc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: 在此添加存储代码
+	}
+	else
+	{
+		// TODO: 在此添加加载代码
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// 缩略图的支持
+void C实验72Doc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// 修改此代码以绘制文档数据
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// 搜索处理程序的支持
+void C实验72Doc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// 从文档数据设置搜索内容。
+	// 内容部分应由“;”分隔
+
+	// 例如:     strSearchContent = _T("point;rectangle;circle;ole object;")；
+	SetSearchContent(strSearchContent);
+}
+
+void C实验72Doc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = nullptr;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != nullptr)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// C实验72Doc 诊断
+
+#ifdef _DEBUG
+void C实验72Doc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void C实验72Doc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// C实验72Doc 命令
