1d1fbb13d7da5ab9246908b7e51d0a8e58b96df5
Utopi-L <77149479+Utopi-L@users.noreply.github.com>
2025-02-24 17:28:38 +0800

    new file:   Windows Program Design/Test 9/7-2Doc.h

---
+++ Windows Program Design/Test 9/7-2Doc.h
@@ -0,0 +1,49 @@
+﻿
+// 实验7-2Doc.h: C实验72Doc 类的接口
+//
+
+
+#pragma once
+
+
+class C实验72Doc : public CDocument
+{
+protected: // 仅从序列化创建
+	C实验72Doc() noexcept;
+	DECLARE_DYNCREATE(C实验72Doc)
+
+// 特性
+public:
+	CRect m_rect;
+// 操作
+public:
+
+// 重写
+public:
+	virtual BOOL OnNewDocument();
+	virtual void Serialize(CArchive& ar);
+#ifdef SHARED_HANDLERS
+	virtual void InitializeSearchContent();
+	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
+#endif // SHARED_HANDLERS
+
+// 实现
+public:
+	virtual ~C实验72Doc();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	
+	DECLARE_MESSAGE_MAP()
+
+#ifdef SHARED_HANDLERS
+	// 用于为搜索处理程序设置搜索内容的 Helper 函数
+	void SetSearchContent(const CString& value);
+#endif // SHARED_HANDLERS
+};
