bf71b2aae0e177793cac3f7147cd9732dff336e4
Utopi-L <77149479+Utopi-L@users.noreply.github.com>
2025-02-28 11:56:57 +0800

    new file:   C++ Primer solutions/1/1.1/1.cpp 	new file:   C++ Primer solutions/1/1.1/1.exe 	new file:   C++ Primer solutions/1/1.2/1-2.cpp 	new file:   C++ Primer solutions/1/1.2/1-2.exe 	new file:   C++ Primer solutions/1/1.3/1.3.cpp 	new file:   C++ Primer solutions/1/1.3/1.3.exe 	new file:   C++ Primer solutions/1/1.4/1.4.cpp 	new file:   C++ Primer solutions/1/1.4/1.4.exe 	new file:   C++ Primer solutions/1/1.5/1.5.cpp 	new file:   C++ Primer solutions/1/1.6/1.6.cpp

---
+++ C++ Primer solutions/1/1.4/1.4.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main()
+{
+    double a,b;
+    std::cin >>a>>b;
+    std::cout<<a*b;
+}
---
+++ C++ Primer solutions/1/1.6/1.6.cpp
@@ -0,0 +1,6 @@
+{
+    std::cout << "The sum of" << v1;
+    << "and" << v2;
+    << "is" << v1 + v2 << std::endl;
+}
+// 缺乏iostream头文件
