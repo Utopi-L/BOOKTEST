852bfebf0268f7dca47ea33051390a471b43136a
Utopi-L <77149479+Utopi-L@users.noreply.github.com>
2025-03-07 15:48:41 +0800

    new file:   C++ Primer solutions/2/2.7/2.7.cpp

---
+++ C++ Primer solutions/2/2.7/2.7.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"who goes with F\145rgus?\012"<<endl;
+    cout<<typeid(.14e1L).name()<<endl;
+    cout<<typeid(1024.f).name()<<endl;
+    cout<<typeid(3.14L).name()<<endl;
+    return 0;
+
+}
